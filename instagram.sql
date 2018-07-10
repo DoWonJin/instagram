bc5d61530baa651c600f183112f7fbdc34283a9a
DoWonJin <ehdnjswls3@naver.com>
2018-07-10 11:09:26 +0100

    Add files via upload

--- instagram.sql
+++ instagram.sql
@@ -1,3 +1,5 @@
+CREATE DATABASE  IF NOT EXISTS `instagram` /*!40100 DEFAULT CHARACTER SET utf8 */;
+USE `instagram`;
 -- MySQL dump 10.13  Distrib 5.7.9, for Win64 (x86_64)
 --
 -- Host: localhost    Database: instagram
@@ -170,4 +172,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2018-04-01 13:23:51
+-- Dump completed on 2018-03-09 22:23:18
